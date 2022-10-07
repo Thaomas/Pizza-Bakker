fda3cbe353fe9a3443bb199af9d448ecfacff781
Redouan <redouan778@gmail.com>
2022-10-07 13:49:04 +0200

    File: IO.cs

--- PizzaBakker/Pizza_Server/Logic/Connections/IO.cs
+++ PizzaBakker/Pizza_Server/Logic/Connections/IO.cs
@@ -13,18 +13,6 @@ namespace REI_Server.Logic.Connections
     {
 
         private static readonly string dir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-        public static void WriteFile(string fileName, string fileExtension, string content, bool append = false)
-        {
-            string path = $"{dir}{"\\"}{fileName}{fileExtension}";
-
-            if (append)
-            {
-                File.AppendAllText(path, content);
-                return;
-            }
-
-            File.WriteAllText(path, content);
-        }
 
         public static void WriteFile(string fileName, string content)
         {
@@ -33,17 +21,7 @@ namespace REI_Server.Logic.Connections
             File.WriteAllText(path, content);
         }
 
-        public static void WriteNewTextToFile(string fileName, string content)
-        {
-            string path = $"{dir}{"\\"}{fileName}";
-            if (File.Exists(path))
-            {
-                File.WriteAllText(path, content);
-                return;
-            }
-        }
-
-        public static T ReadObjectFromFile<T>(string fileName)
+        /*public static List<ICommon> ReadObjectFromFile<T>(string fileName)
         {
             string path = $"{dir}{"\\"}{fileName}";
 
@@ -51,9 +29,7 @@ namespace REI_Server.Logic.Connections
             {
                 return default(T);
             }
-
-            return JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
-        }
+        }*/
 
         public static string ReadFile(string fileName)
         {
@@ -65,23 +41,6 @@ namespace REI_Server.Logic.Connections
             }
 
             return File.ReadAllText(path);
-        }
-
-        public static void SaveEmployees(Dictionary<uint, Employee> dic)
-        {
-            WriteFile("SaveData\\Employees", ".json", JsonConvert.SerializeObject(dic, Formatting.Indented));
-        }
-
-        public static Dictionary<uint, Employee> LoadEmployees()
-        {
-            string jsonString = ReadFile("SaveData\\Employees.json");
-
-            if (jsonString == null)
-            {
-                return new Dictionary<uint, Employee>();
-            }
-
-            return JsonConvert.DeserializeObject<Dictionary<uint, Employee>>(jsonString);
-        }
+        }     
     }
 }
