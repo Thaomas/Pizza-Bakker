6941fc66f1b11b3a86593d02b0a6d795db75fa1b
Redouan <redouan778@gmail.com>
2022-10-07 13:46:35 +0200

    file: Warehouse.cs

--- PizzaBakker/Pizza_Server/Logic/Warehouse/Warehouse.cs
+++ PizzaBakker/Pizza_Server/Logic/Warehouse/Warehouse.cs
@@ -5,37 +5,147 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace REI_Server.Logic.Warehouse
 {
     internal class Warehouse
     {
-        private Dictionary<uint, WarehouseItem> _ingredients;
+    private Dictionary<uint, WarehouseItem> _ingredientsx;
+        private List<WarehouseItem> _ingredients = new();
 
         private readonly string _fileName = $"ingredients";
+
 
         public Warehouse()
         {
             LoadFromFile();
         }
 
+        public void startSavingThread()
+        {
+            new Thread(SavingLoop).Start();
+        }
+
+        private void SavingLoop()
+        {
+            while (true)
+            {
+                Thread.Sleep(3000);
+
+                string serializeData = JsonConvert.SerializeObject(_ingredients, Formatting.Indented);
+                IO.WriteFile("Warehouse.json", serializeData);
+
+                Console.WriteLine("saving loop !");
+            }
+        }
+
+        public void orderPizza(List<string> orderPizza)
+        {
+            bool _orderComplete = true;
+            List<string> _outOfStockIngredients = new();
+
+            foreach (string singleIngedient in orderPizza)
+            {
+                if (!decrementIngredient(singleIngedient))
+                {
+                    _orderComplete = false;
+                    if (!_outOfStockIngredients.Contains(singleIngedient))
+                    {
+                        _outOfStockIngredients.Add(singleIngedient);
+                    }
+                    
+                }                              
+            }
+
+            if (_orderComplete)
+            {
+                Console.WriteLine("Bestelling is klaar gemaakt!");
+            }
+            else
+            {
+                Console.WriteLine("De bestelling kon niet afgemaatk worder ern missen een aantal ingredienten zoals: ");
+
+                foreach (string singleIngedient in _outOfStockIngredients)
+                {
+                    Console.WriteLine(singleIngedient);
+                }
+            }
+        }
+
+        //WERKT!!
+        public bool decrementIngredient(string singleIngredient)
+        {
+            
+            WarehouseItem retrievedIngredient = _ingredients.First(name => name.Ingredient.Name.Equals(singleIngredient));
+
+            if (retrievedIngredient.Count > 0)
+            {
+                retrievedIngredient.Count -= 1;
+
+                _ingredients.Remove(retrievedIngredient);
+                _ingredients.Add(retrievedIngredient);
+                 return true;
+            }
+            else
+            {
+                Console.WriteLine("Het product: " + retrievedIngredient.Ingredient.Name + " is uitverkocht!");
+                return false;
+            }                
+        }
+                    
+
+        //Werkt!
         public void AddIngredient(WarehouseItem item)
         {
-            _ingredients.Add(item.Ingredient.Id, item);
+            bool isExist = _ingredients.Exists(ti => ti.Ingredient.Name.Equals(item.Ingredient.Name));
+
+            if (isExist)
+            {
+                Console.WriteLine("bestaat al sorry");
+                return;
+            }
+            _ingredients.Add(item);
         }
 
-        public void SaveToFile()
+        //WERKT!
+        public void DeleteIngredient(string item)
         {
-
+            try
+            {
+                WarehouseItem deletedItem = _ingredients.First(name => name.Ingredient.Name.Equals(item));
+                _ingredients.Remove(deletedItem);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e + "Naam bestaat niet, dus hebben wij het niet kunnen verwijderen!!");
+            }
         }
 
-        private void LoadFromFile()
+        //WERKT!
+        public WarehouseItem RetrieveIngredient(string item)
         {
-            _ingredients = IO.ReadObjectFromFile<Dictionary<uint, WarehouseItem>>(_fileName);
 
-            if (_ingredients == null)
-                _ingredients = new Dictionary<uint, WarehouseItem>();
+            WarehouseItem foundIngredient = null;
+
+            try {
+                foundIngredient = _ingredients.First(name => name.Ingredient.Name.Equals(item));
+            } catch (Exception e) {
+                Console.WriteLine(e + "Naam bestaat niet, dus hebben wij het niet kunnen verwijderen!!");
+            }
+
+            return foundIngredient;
+        }
+
+
+        public void LoadFromFile() {          
+
+            _ingredients = JsonConvert.DeserializeObject<List<WarehouseItem>>(IO.ReadFile("Warehouse.json"));
+
+            if (_ingredients == null) {
+                Console.WriteLine("Geen ingredienten beschikbaar!");
+            }
+                        
         }
     }
 }
