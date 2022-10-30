e85c11cfca55048cd0c92ecb4fdc87c66c3b2f62
Redouan <redouan778@gmail.com>
2022-10-30 16:55:33 +0100

    Added one more test

---
+++ PizzaBakker/Shared/Employee.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Shared
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return $"{this.FirstName} {this.LastName}"; } }
+        public uint WorkId { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string Department { get; set; }
+        public string Password { get; set; }
+
+        public Employee(string firstName, string lastName, uint workId, DateTime? birthday, string department, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            WorkId = workId;
+            Birthday = birthday;
+            Department = department;
+            Password = password;
+        }
+    }
+}
---
+++ PizzaBakker/Test/TestPizza.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shared;
+
+namespace Test
+{
+    public class TestPizza
+    {
+        private Pizza _pizzaSalami;
+
+        private Pizza _pizzaPollo;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _pizzaSalami = new Pizza()
+            {
+                Name = "Pizza Salami",
+                Ingredients = new List<uint>() {1,2,3,4}
+            };
+            
+            _pizzaPollo = new Pizza()
+            {
+                Name = "Pizza Pollo",
+                Ingredients = new List<uint>() { 1,2,3,6,7}
+            };
+        }
+
+        [Test]
+        public void Test_Pizza_Pollo_Name()
+        {
+            Assert.AreEqual(_pizzaPollo.Name,"Pizza Pollo");
+        }
+        
+        [Test]
+        public void Test_Pizza_Salami_Name()
+        {
+            Assert.AreEqual(_pizzaSalami.Name,"Pizza Salami");
+        }
+        
+        [Test]
+        public void Test_Ingredient_More_Than_Pizza_Salami()
+        {
+            Assert.Greater(_pizzaPollo.Ingredients.Count, _pizzaSalami.Ingredients.Count);
+        }
+    }
+}
